d3178ad89cf5f0519107110897c4fbacc299fff0
renancvitor <renan.vitor.cm@gmail.com>
2025-06-04 16:56:44 -0300

    Implementação cancelamento de consultas

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsulta.java
@@ -33,7 +33,7 @@ public class AgendaDeConsulta {
 
         var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
-        var consulta = new Consulta(null, medico, paciente, dados.data());
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
     }
 
@@ -47,5 +47,14 @@ public class AgendaDeConsulta {
         }
 
         return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informado não existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
     }
 }
